111e50874c5ae52ab92b7303eff03c4c623cf7e2
Stan Mendonca <stan_mendonca@hotmail.com>
2019-07-08 23:59:02 -0700

    Added test for GetRepository

---
+++ FrameworkExtender/EARepositoryTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EARepositoryTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var repository = Repository.Helpers.Repository.GetRepository(args[0]);
+
+            try
+            {
+                var instance = string.Format($"Instance Guid: {repository.InstanceGUID}");
+                Console.WriteLine(instance);
+                repository.WriteOutput("Script", instance, 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
