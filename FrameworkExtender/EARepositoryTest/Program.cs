8ab6484b8f8e1fc573ea52da7f214523f7a04488
smend5464 <stan_mendonca@hotmail.com>
2019-09-12 12:52:18 -0700

    Updated EARepositoryTest to display the pid, instanceGUID and Connectionstring

--- FrameworkExtender/EARepositoryTest/Program.cs
+++ FrameworkExtender/EARepositoryTest/Program.cs
@@ -8,12 +8,16 @@ namespace EARepositoryTest
         {
             try
             {
-                var repository = Repository.Helpers.Repository.GetRepository(args[0]);
+//                var repository = Repository.Helpers.Repository.GetRepository(args[0]);
                 var openRepositories = Repository.Helpers.Repository.GetOpenRepositories();
                 
                 foreach (var rep in openRepositories)
                 {
-                    repository.WriteOutput("Script", rep.ConnectionString, 0);
+//                    repository.WriteOutput("Script", rep.ConnectionString, 0);
+                    var pid = rep.processID;
+                    var repository = rep.repository;
+                    Console.WriteLine("Process ID: {0}, InstanceGUID: {1}, Repository: {2}", pid,
+                        repository.InstanceGUID, repository.ConnectionString);
                 }
             }
             catch (Exception e)
